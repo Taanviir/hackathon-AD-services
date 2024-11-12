7a31094b9e7fd21446832416ea64fbc196e6bb1e
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-11-13 01:24:28 +0400

    feat: set up backend view to use simple AI model

--- backend/opinion_requests/views.py
+++ backend/opinion_requests/views.py
@@ -1,16 +1,15 @@
-# import spacy
+import spacy
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 # Load English tokenizer, tagger, parser and NER
-# nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 
 
 class DocumentProcessingView(APIView):
     def post(self, request):
-        # document_text = request.data.get("document")
-        # doc = nlp(document_text)
-        # summary = " ".join([sent.text for sent in doc.sents][:3])
-        summary = "this is goodo"
+        document_text = request.data.get("document")
+        doc = nlp(document_text)
+        summary = " ".join([sent.text for sent in doc.sents][:3])
         return Response({"summary": summary}, status=status.HTTP_200_OK)
